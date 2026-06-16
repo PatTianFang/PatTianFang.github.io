b730f3d06f9c9fddb82612f651f7275615d43b3d
PatTianFang <2541620285@qq.com>
2026-06-16 10:36:37 +0800

    Publish notes 2026-06-16 10:36:36

--- js/records.js
+++ js/records.js
@@ -326,9 +326,10 @@
                     scrollWheelZoom: false,
                     zoomControl: true,
                 });
-                window.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                window.L.tileLayer('https://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
+                    subdomains: ['1', '2', '3', '4'],
                     maxZoom: 18,
-                    attribution: '&copy; OpenStreetMap',
+                    attribution: '&copy; 高德地图',
                 }).addTo(recordMap);
                 markerLayer = window.L.layerGroup().addTo(recordMap);
                 mapReady = true;
